627fd4a57e46663545cbfb83c4ba71e2e1980f46
MinkinMS <71133296+Minkinms@users.noreply.github.com>
2025-04-02 10:36:09 +0300

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
-
-if [ "$ENV" = 'DEV' ]; then
-  echo "Running Development Server"
-  exec python "identidock.py"
-elif [ "$ENV" = 'UNIT' ]; then
-  echo "Running Unit Tests"
-  exec python "tests.py"
+echo "====== Начало скрипта"
+echo $ENV
+if [ $ENV = "DEV" ]; then
+	echo "====== Условие DEV выполнено" 
+	exec python "identidock.py"
+elif [ $ENV = "UNIT" ]; then
+	echo "====== Условие UNIT выполнено" 
+	exec python "tests.py"
 else
-  echo "Running Production Server"
-  exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
-             --callable app --stats 0.0.0.0:9191
+	echo "====== Условие НЕ выполнено"
+	exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
 fi
